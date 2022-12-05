6ceeff9372ccf5a81621e2299300ace2f1377a3c
Wenceslao <wenceslao.reyes@msw.com.mx>
2022-12-05 13:02:19 -0600

    View create genre form

---
+++ Client/Pages/Genre/CreateGenre.razor.cs
@@ -0,0 +1,14 @@
+﻿using BlazorPeliculas.Shared.Entities;
+
+namespace BlazorPeliculas.Client.Pages.Genre
+{
+    public partial class CreateGenre
+    {
+        private GenreEntity Genre = new();
+
+        public void Create()
+        {
+            Console.WriteLine($"Genre: {Genre.Description}");
+        }
+    }
+}
